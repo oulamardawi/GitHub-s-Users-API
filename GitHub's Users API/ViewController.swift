d7fb8a9a442a20740a6316abc8eee5a1f4ebe30b
Mardawi <ola.mardawi@gmail.com>
2022-04-27 17:50:09 +0300

    getData from API url
    declare url, then call getData function from that url which initiate a request using URLSession.shared.dataTask then convert it to specific format( results struct) contains all url property by using codable protocol

--- GitHub's Users API/ViewController.swift
+++ GitHub's Users API/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = "https://api.github.com/users?since=0"
+        getData(from: url)
     }
-
-
+    
+    private func getData(from url: String) {
+        
+        //Request
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("sth is nil")
+                return
+            }
+            //have data :convert it to struct
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+              }
+            catch {
+                print("failed to convert \(error.localizedDescription)")
+               }
+            guard let json = result else {
+                return
+               }
+            print ("\(json.results.login)")
+        })
+        task.resume()
+    }
+    //codable protocol: convert data we get from url to struct
+    struct Response: Codable {
+        let results: myResults
+        
+    }
+    struct myResults: Codable {
+        let login: String
+        let id: Int
+        let node_id: String
+        let avatar_url: String
+        let gravatar_id: String
+        let url: String
+        let html_url: String
+        let followers_url: String
+        let following_url: String
+        let gists_url: String
+        let starred_url: String
+        let subscriptions_url: String
+        let organizations_url: String
+        let repos_url: String
+        let events_url: String
+        let received_events_url: String
+        let type: String
+        let site_admin: Bool
+    }
+    
 }
 
